df7582261125711d714de3a5978419db34df2e11
YalaCom <tomsa_tomsa@mail.ru>
2026-08-01 20:24:09 +0300

    api.js

--- api.js
+++ api.js
@@ -7,6 +7,7 @@ export async function rpc(functionName, args = {}) {
   try {
     const response = await fetch(`${SUPABASE_URL}/rest/v1/rpc/${functionName}`, {
       method: "POST",
+      cache: "no-store",
       headers: {
         apikey: SUPABASE_PUBLISHABLE_KEY,
         Authorization: `Bearer ${SUPABASE_PUBLISHABLE_KEY}`,
